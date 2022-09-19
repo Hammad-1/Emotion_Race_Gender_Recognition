fe92f3d7e4edde42fc3c9d866fd38e1cc299d10a
Hammad-1 <affannaveed4124@gmail.com>
2022-09-20 02:22:40 +0500

    modified for deployment commit

--- Emotion_Race_Gender_REcognition/settings.py
+++ Emotion_Race_Gender_REcognition/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [
- "twitertool.herokuapp.com", "localhost", "127.0.0.1"
+ "HammadNaveedElevateAITask.herokuapp.com", "localhost", "127.0.0.1"
 ]
 
 # Application definition
